795c99fbbf4b8552b89b2b01855fa7599144207f
Jossy-Jones <jossyjones187@gmail.com>
2021-10-29 16:28:17 +0100

    Start deployment to netlify

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,32 +7,33 @@
 "use strict"
 
 const App = (function app() {
-    function stickyHeader(){
+    // Sticky header
+    function stickyHeader() {
         let premier = document.querySelector("section.premier");
         let header = document.querySelector("#app>header");
-        if(premier){
-            window.addEventListener("scroll", function(e){
-                if(window.scrollY > premier.scrollHeight){
+        if (premier) {
+            window.addEventListener("scroll", function (e) {
+                if (window.scrollY > premier.scrollHeight) {
                     header.classList.add("fixed");
-                    document.querySelector(".logo img").setAttribute("src","../assets/img/logo.png");
+                    document.querySelector(".logo img").setAttribute("src", "../assets/img/logo.png");
                 } else {
                     header.classList.remove("fixed");
-                    document.querySelector(".logo img").setAttribute("src","../assets/img/logo-alt.png")
+                    document.querySelector(".logo img").setAttribute("src", "../assets/img/logo-alt.png")
                 };
             })
         }
     };
 
-    function alertMessages(type,message){
-        type = typeof(type) == "string" && ["success","danger","neutral"].indexOf(type) > -1 ? type : "neutral";
-        message = typeof(message) == "string" && message.trim().length > 0 ? message.trim() : 'No message';
+    function alertMessages(type, message) {
+        type = typeof (type) == "string" && ["success", "danger", "neutral"].indexOf(type) > -1 ? type : "neutral";
+        message = typeof (message) == "string" && message.trim().length > 0 ? message.trim() : 'No message';
 
         // Alert Template
         let alertTemplate = document.createElement("div");
         alertTemplate.classList.add(`${type}-alert`);
         alertTemplate.innerHTML = `<p>${message}</p>`;
 
-        if(type && message){
+        if (type && message) {
             // Check alert type
             document.querySelector("body").append(alertTemplate);
         }
@@ -41,35 +42,31 @@ const App = (function app() {
         }, 10000);
     };
 
-    function upgamesDropdown(){
+    function upgamesDropdown() {
         let gameCats = document.querySelectorAll(".category[data-category]>._title");
-        gameCats.forEach(gameCat=>{
-            gameCat.addEventListener("click",(e)=>{
+        gameCats.forEach(gameCat => {
+            gameCat.addEventListener("click", (e) => {
                 let toggle = gameCat.parentElement.getAttribute("data-toggle");
-                if(toggle == "open"){
-                    gameCat.parentElement.setAttribute("data-toggle","close");
+                if (toggle == "open") {
+                    gameCat.parentElement.setAttribute("data-toggle", "close");
                 }
-                if(toggle == "close"){
-                    gameCat.parentElement.setAttribute("data-toggle","open");
+                if (toggle == "close") {
+                    gameCat.parentElement.setAttribute("data-toggle", "open");
                 }
             })
         })
     }
 
     //@Todo Remove this code and create your custom form validation
-    function formValidation(){
-        let forms = document.querySelectorAll("form");
-        forms.forEach(form=>{
-            form.addEventListener("submit",e=>{
-                e.preventDefault();
-                let valid = form.checkValidity();
-                console.log(valid);
-                if(valid){
-                    alertMessages("success","submitted successfully!");
-                } else {
-                    alertMessages("danger", "failed!");
-                }
-            })
+    function formValidation(form) {
+        form.addEventListener("submit", e => {
+            e.preventDefault();
+            let valid = form.checkValidity();
+            if (valid) {
+                alertMessages("success", "submitted successfully!");
+            } else {
+                alertMessages("danger", "failed!");
+            }
         })
     }
 
